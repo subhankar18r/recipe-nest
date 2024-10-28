586d0ab188339397d232745555529d551e8d8c52
Subhankar Rajbanshi <subhankar18r@gmail.com>
2024-10-28 22:27:49 +0530

    url fixed

--- js/index.js
+++ js/index.js
@@ -8,12 +8,12 @@ const searchInput = document.querySelector("input[type=search]");
 searchInput.addEventListener("keydown", (e) => {
   if (!e.target.value) return;
   if (e.key === "Enter") {
-    location.href = `/search.html?q=${e.target.value}`;
+    location.href = `./search.html?q=${e.target.value}`;
   }
 });
 
 window.goToSearch = function () {
   const query = document.querySelector("input[type=search]").value;
   if (!query) return;
-  location.href = `/search.html?q=${query}`;
+  location.href = `./search.html?q=${query}`;
 };
